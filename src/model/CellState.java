034db80e58e7650402cd6e6a3a5e73b34bac95f3
Tommy Coller <tcollier.b25@gmail.com>
2024-04-11 18:39:05 -0600

    Added ViewController color changing, began ZombieLife implementation

--- src/model/CellState.java
+++ src/model/CellState.java
@@ -4,5 +4,5 @@ package model;
  * A cell state. Add more later?
  */
 public enum CellState {
-    DEAD, ALIVE;
+    DEAD, ALIVE, ZOMBIE;
 }
--- src/model/ZombieLife.java
+++ src/model/ZombieLife.java
@@ -5,12 +5,7 @@ import java.util.Arrays;
 import edu.princeton.cs.algs4.Graph;
 import edu.princeton.cs.algs4.Queue;
 
-/**
- * A version of Conway's Game of Life that uses a graph to represent the neighbor relations for each cell.
- * Instead of the neighbor cells being those sharing an edge and corner, neighbors are the 8 cells a knight's
- * move away from each cell.
- */
-public class KnightLife implements ILife {
+public class ZombieLife implements ILife {
 	private Graph world;
 	private CellState[] cells; // row-col indexed
 	private int nrows;
@@ -70,21 +65,16 @@ public class KnightLife implements ILife {
 		int row = convertToRow(index);
 		int col = convertToCol(index);
 
-		// int[] rowOffsets = {(row - 1 + nrows) % nrows, row, (row + 1 + nrows) % nrows};
-		// int[] colOffsets = {(col - 1 + ncols) % ncols, col, (col + 1 + ncols) % ncols};
-		int[] rowOffsets = { (row - 2 + nrows) % nrows, (row - 1 + nrows) % nrows, (row + 1 + nrows) % nrows,
-				(row + 2 + nrows) % nrows };
-		int[] colOffsets = { (col - 2 + ncols) % ncols, (col - 1 + ncols) % ncols, (col + 1 + ncols) % ncols,
-				(col + 2 + ncols) % ncols };
-		int[][] neighbors = { { rowOffsets[0], colOffsets[1] }, { rowOffsets[0], colOffsets[2] },
-				{ rowOffsets[1], colOffsets[0] }, { rowOffsets[1], colOffsets[3] }, { rowOffsets[2], colOffsets[0] },
-				{ rowOffsets[2], colOffsets[3] }, { rowOffsets[3], colOffsets[1] }, { rowOffsets[3], colOffsets[2] } };
+		int[] rowOffsets = {(row - 1 + nrows) % nrows, row, (row + 1 + nrows) % nrows};
+        int[] colOffsets = {(col - 1 + ncols) % ncols, col, (col + 1 + ncols) % ncols};
 
-		for (int n = 0; n < neighbors.length; n++) {
-			int neighbor = convertToIndex(neighbors[n][0], neighbors[n][1]);
-			if (hasEdge(index, neighbor) != true)
-				world.addEdge(index, neighbor);
-		}
+        for (int r : rowOffsets)
+    		for (int c : colOffsets)
+    			if (r != row || c != col) { // Disclude current cell
+    				int neighbor = convertToIndex(r, c);
+    				if (hasEdge(index, neighbor) != true)
+    					world.addEdge(index, neighbor);
+    			}
 	}
 
 	/**
@@ -110,9 +100,9 @@ public class KnightLife implements ILife {
 	public void randomize() {
 		for (int current = 0; current < cells.length; current++)
 			if (RANDOM.nextBoolean())
-				cells[current] = CellState.ALIVE;
-			else
-				cells[current] = CellState.DEAD;
+                cells[current] = CellState.ZOMBIE;
+            else
+            	cells[current] = CellState.DEAD;
 	}
 
 	@Override
@@ -131,12 +121,12 @@ public class KnightLife implements ILife {
 
 		// Calculate needed updates
 		for (int current = 0; current < cells.length; current++) {
-			// Count amount of alive neighbors
-			int aliveNeighbors = 0;
+    		// Count amount of alive neighbors
+    		int aliveNeighbors = 0;
 			for (int neighbor : world.adj(current)) {
-				if (cells[neighbor] == CellState.ALIVE)
-					aliveNeighbors++;
-			}
+    			if (cells[neighbor] == CellState.ALIVE)
+    				aliveNeighbors++;
+    		}
 
 			// Record needed updates
 			int row = convertToRow(current);
@@ -145,45 +135,46 @@ public class KnightLife implements ILife {
 			if (cells[current] == CellState.ALIVE) {
 				if (aliveNeighbors < 2 || aliveNeighbors > 3) // Alive cells only stay alive if between 2-3 neighbors.
 					queue.enqueue(new Cell(row, col, CellState.DEAD));
-			} else { // if (cells[i].state() == CellState.DEAD)
+			}
+			else { // if (cells[i].state() == CellState.DEAD)
 				if (aliveNeighbors == 3) // Dead cell with 3 neighbors becomes alive.
 					queue.enqueue(new Cell(row, col, CellState.ALIVE));
 			}
-		}
+    	}
 
 		boolean worldChanged = false;
 
-		// Make needed updates (done afterwards to prevent invalid updates)
-		while (!queue.isEmpty()) {
-			Cell cell = queue.dequeue();
+    	// Make needed updates (done afterwards to prevent invalid updates)
+    	while (!queue.isEmpty()) {
+    		Cell cell = queue.dequeue();
 
-			// Invoke callback if a new state differs from old state
-			if (cell.state() != get(cell.row(), cell.col())) {
-				action.invoke(cell.row(), cell.col(), cell.state());
-				worldChanged = true;
-			}
+            // Invoke callback if a new state differs from old state
+            if (cell.state() != get(cell.row(), cell.col())) {
+                action.invoke(cell.row(), cell.col(), cell.state());
+                worldChanged = true;
+            }
 
-			set(cell.row(), cell.col(), cell.state());
-		}
+    		set(cell.row(), cell.col(), cell.state());
+    	}
 
-		return worldChanged;
+    	return worldChanged;
 	}
 
 	@Override
 	public void forAllLife(Callback action) {
 		for (int current = 0; current < cells.length; current++)
-			if (cells[current] == CellState.ALIVE)
-				action.invoke(convertToRow(current), convertToCol(current), cells[current]);
+			if (cells[current] == CellState.ALIVE || cells[current] == CellState.ZOMBIE)
+            	action.invoke(convertToRow(current), convertToCol(current), cells[current]);
 	}
 
 	@Override
 	public long populationCount() {
-		long count = 0;
+	    long count = 0;
 
-		for (var state : cells)
-			if (state == CellState.ALIVE)
-				count++;
+	    for (var state : cells)
+	        if (state == CellState.ALIVE)
+	            count++;
 
-		return count;
+	    return count;
 	}
 }
